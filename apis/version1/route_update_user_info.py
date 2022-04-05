e3c9c1ba65be3a89e5d3a9c2b2dfd887624ca66f
bhavinsen01 <bhavinsen23@gmail.com>
2022-04-05 14:43:06 +0530

    fix the issue

--- apis/version1/route_update_user_info.py
+++ apis/version1/route_update_user_info.py
@@ -2,7 +2,7 @@ from db.models.users import User
 from fastapi import APIRouter
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from schemas.users import ShowUser, UserCreate
+from schemas.users import ShowUser, UpdateUser 
 from db.session import engine
 
 
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.put("/{user_id}", response_model=ShowUser)
-def update_user(user_id: int, user: UserCreate):
+def update_user(user_id: int, user: UpdateUser):
     with Session(engine) as session:
         db_user = session.get(User, user_id)
         if not db_user:
